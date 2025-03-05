f067af6ce186ca687fb4331a4a5d381a6d06a069
govind156 <govindmadaan156@gmail.com>
2025-03-05 11:21:08 +0530

    add loader and user state inside redux folder for state management

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,9 +1,8 @@
-import { configureStore } from "@reduxjs/toolkit";
-import loaderReducer from './loaderSlice';
-import userReducer from './usersSlice';
-
-const store = configureStore({
-    reducer: { loaderReducer, userReducer }
-});
+import {configureStore} from '@reduxjs/toolkit'
+import loaderReducer from './loaderSlice'
+import userReducer from './usersSlice'
+const store=configureStore({
+    reducer:{loaderReducer,userReducer}
+})
 
 export default store;
--- client/src/redux/usersSlice.js
+++ client/src/redux/usersSlice.js
@@ -1,20 +1,11 @@
-import { createSlice } from "@reduxjs/toolkit";
-
-const usersSlice = createSlice({
-    name: 'user',
-    initialState: { 
-        user: null, 
-        allUsers: [],
-        allChats: [],
-        selectedChat: null
-    },
-    reducers: {
-        setUser: (state, action) => { state.user = action.payload; },
-        setAllUsers: (state, action) => { state.allUsers = action.payload; },
-        setAllChats: (state, action) => { state.allChats = action.payload; },
-        setSelectedChat: (state, action) => { state.selectedChat = action.payload; }
+import {createSlice} from '@reduxjs/toolkit'
+const userSlice=createSlice({
+    name:'user',
+    initialState:{user:null},
+    reducers:{
+        setUser:(state,action)=>{state.user=action.payload}
     }
-});
+})
+export const {setUser}=userSlice.actions
+export default userSlice.reducer;
 
-export const { setUser, setAllUsers,  setAllChats, setSelectedChat} = usersSlice.actions;
-export default usersSlice.reducer;
